8b488a59071a60c7d6801e7c6f18f245b9479524
randunu <randu.ranga@gmail.com>
2016-09-06 23:44:00 +0530

    Adding JPA Support - 20160906

---
+++ src/main/java/com/goldbows/model/Timezones.java
@@ -0,0 +1,16 @@
+package com.goldbows.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * Created by goldbows on 9/6/2016.
+ */
+
+@Entity
+@Table(name = "T_TIME_ZONES")
+public class Timezones implements Serializable{
+
+}
+
